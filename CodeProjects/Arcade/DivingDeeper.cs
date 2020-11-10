a7765ec4545d59ad93faf684d480742ae8358e66
Sticcia <davidercosta@gmail.com>
2020-11-10 12:29:47 +0100

    Organize C# arcade

---
+++ CodeProjects/Arcade/DivingDeeper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeProjects.Arcade
+{
+	class DivingDeeper
+	{
+		int[] extractEachKth(int[] inputArray, int k)
+		{
+			int[] result = new int[inputArray.Length - inputArray.Length / k]; ;
+			var j = 0;
+
+			for (int i = 1; i < inputArray.Length + 1; i++)
+			{
+				if (i % k != 0)
+				{
+					result[j] = inputArray[i - 1];
+					j++;
+				}
+			}
+
+			return result;
+
+			/*
+			 * return inputArray.Where((e, i) => (i + 1) % k != 0).ToArray();
+			 */
+		}
+
+		char firstDigit(string inputString)
+		{
+			return inputString.First(e => char.IsDigit(e));
+		}
+
+		int differentSymbolsNaive(string s)
+		{
+			return s.Distinct().Count();
+		}
+
+		int arrayMaxConsecutiveSum(int[] inputArray, int k)
+		{
+			var max = 0;
+
+			for (int i = 0; i < inputArray.Length - k; i++)
+			{
+				var aux = inputArray.Skip(i).Take(k).Sum();
+				if (aux > max)
+				{
+					max = aux;
+				}
+			}
+
+			return max;
+		}
+	}
+}
--- CodeProjects/Arcade/EdgeOfTheOcean.cs
+++ CodeProjects/Arcade/EdgeOfTheOcean.cs
@@ -1,35 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace CodeProjects
+namespace CodeProjects.Arcade
 {
-	class Arcade1
+	class EdgeOfTheOcean
 	{
-		int centuryFromYear(int year)
-		{
-			int result = year / 100;
-
-			return year % 100 == 0 ? result : result + 1;
-		}
-
-		bool checkPalindrome(string inputString)
-		{
-			int j = inputString.Length;
-
-			for (int i = 0; i < inputString.Length; i++)
-			{
-				j--;
-				if (inputString[i] != inputString[j])
-				{
-					return false;
-				}
-			}
-
-			return true;
-		}
-
 		int adjacentElementsProduct(int[] inputArray)
 		{
 			int result = int.MinValue;
