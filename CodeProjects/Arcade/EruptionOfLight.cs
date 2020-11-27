c791fac752df01ff6b0d94f03c4306c546490695
Sticcia <davidercosta@gmail.com>
2020-11-27 15:15:11 +0100

    Add EruptionOfLight and RainbowOfClarity

---
+++ CodeProjects/Arcade/EruptionOfLight.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeProjects.Arcade
+{
+	class EruptionOfLight
+	{
+		bool isBeautifulString(string inputString)
+		{
+			char[] letters = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+			int count = Int32.MaxValue;
+
+			foreach (char c in letters)
+			{
+				int aux = inputString.Count(e => e == c);
+
+				if (aux > count)
+				{
+					return false;
+				}
+
+				count = aux;
+			}
+
+			return true;
+		}
+
+		string findEmailDomain(string address)
+		{
+			return address.Split('@').Last();
+		}
+
+		string buildPalindrome(string st)
+		{
+			int len = st.Length;
+			int i = 0;
+
+			while (st != string.Concat(st.Reverse()))
+			{
+				st = st.Insert(len, st[i++].ToString());
+			}
+
+			return st;
+		}
+
+		int electionsWinners(int[] votes, int k)
+		{
+			int m = votes.Max();
+			if (votes.Count(x => x == m) == 1 && k == 0)
+				return 1;
+			else
+				return votes.Count(x => x + k > m);
+		}
+
+		bool isMAC48Address(string inputString)
+		{
+			Regex rgx = new Regex(@"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
+			return rgx.IsMatch(inputString);
+		}
+	}
+}
---
+++ CodeProjects/Arcade/RainbowOfClarity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeProjects.Arcade
+{
+	class RainbowOfClarity
+	{
+		bool isDigit(char symbol)
+		{
+			return char.IsDigit(symbol);
+		}
+
+		string lineEncoding(string s)
+		{
+			string result = "";
+			int count = 0;
+			int curr = 0;
+
+			while (curr < s.Length)
+			{
+				count = s.Skip(curr).TakeWhile(e => e == s[curr]).Count();
+				if (count > 1)
+				{
+					result += count;
+				}
+				result += s[curr];
+				curr += count;
+			}
+
+			return result;
+		}
+
+		int chessKnight(string cell)
+		{
+			int i = Math.Min(cell[0] + 2, 'h') - Math.Max(cell[0] - 2, 'a');
+			int j = Math.Min(cell[1] + 2, '8') - Math.Max(cell[1] - 2, '1');
+			return (i * j) / 2;
+		}
+
+		int deleteDigit(int n)
+		{
+			string value = n.ToString();
+			int result = 0;
+
+			foreach (var item in value)
+			{
+				var aux = int.Parse(value.Remove(value.IndexOf(item), 1));
+				if (aux > result)
+				{
+					result = aux;
+				}
+				
+			}
+
+			return result;
+		}
+	}
+}
